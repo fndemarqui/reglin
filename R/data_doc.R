e9b2677f9e6b6814af356e3f9b5291df4332bd82
fndemarqui <fndemarqui@gmail.com>
2022-08-21 15:23:43 -0300

    oxigenio + pureza

---
+++ R/data_doc.R
@@ -0,0 +1,36 @@
+
+
+#' Nível de colesterol no sangue
+#'
+#' @name oxigenio
+#' @docType data
+#' @author Fábio N. Demarqui \email{fndemarqui@est.ufmg.br}
+#' @keywords datasets
+#' @description Exemplo: Um engenheiro está interessado em estudar a relação entre a pureza do oxigênio (y) produzido em um processo químico de destilação e o percentagem de hidrocarbonetos (x) presente no condensador principal de uma unidade de destilação.
+#' @format Data frame com 20 linhas e duas variáveis:
+#' \itemize{
+#'   \item percentual: percentual de hidrocarbonetos presentes no condensador principal (covariável)
+#'   \item pureza: purezao do oxigênio (variável resposta)
+#' }
+#' @references  Montgomery, D. C., Runger, G. C. (2015) Estatística Aplicada e Probabilidade para Engenheiros, 5ª. Edição, Rio de Janeiro: LTC.
+#'
+NULL
+
+#' Força de fibras de monofilamento
+#'
+#' @name fibras
+#' @docType data
+#' @author Fábio N. Demarqui \email{fndemarqui@est.ufmg.br}
+#' @keywords datasets
+#' @description Delineamento com um fator fixo e uma covariável conduzido para avaliar se existem diferenças na forças de fibras de monofilamentos produzidas por 3 máquinas.
+#' @format Data frame com 15 linhas e 3 variáveis:
+#' \itemize{
+#'   \item força: força da fibra de monofilamento (variável resposta)
+#'   \item maquina: máquina utilizada na produção das fibras de monofilamento (variável categórica com três níveis; fator de interesse principal)
+#'   \item diametro:  diâmetro da fibra de monofilamento (covariável; interesse secundário)
+#' }
+#' @references  Montgomery, D. C. (2016). Design and Analysis of Experiments, 8ª. Edição, New York: Wiley.
+#'
+NULL
+
+
