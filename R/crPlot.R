30e13bfa6875b2132a7e82eb5d7f4cda895d7d36
fndemarqui <fndemarqui@gmail.com>
2022-12-21 11:11:48 -0300

    crPlot bug correction

--- R/crPlot.R
+++ R/crPlot.R
@@ -4,7 +4,7 @@ crPlots <- function(object){
   mf <- stats::model.frame(object)
   ylabel <- names(mf)[1]
   mf <- mf[, -1, drop = FALSE]
-  coefs <- coef(fit)
+  coefs <- coef(object)
   plots <- list()
   p <- ncol(mf)
   r <- as.data.frame(residuals(object, "partial"))
@@ -12,10 +12,12 @@ crPlots <- function(object){
 
   for(j in 1:p){
     df <- tibble(
-      x = mf[,j],
-      y = r[,j]
+      # x = mf[,j],
+      # y = r[,j]
+      x = mf[,labels[j]],
+      y = r[,labels[j]]
     )
-    if(is.factor(df$x)){
+    if(is.factor(df$x) | is.character(df$x)){
       plots[[j]] <- ggplot(df, aes(x = .data$x, y = .data$y)) +
         geom_boxplot() +
         ylab(paste0("Component + Residual (", ylabel, ")")) +
@@ -25,8 +27,8 @@ crPlots <- function(object){
       names(beta) <- NULL
       plots[[j]] <- ggplot(df, aes(x = .data$x, y = .data$y)) +
         geom_point() +
-        #geom_abline(slope = coefs[labels[j]], color = "red") +
-        geom_abline(slope = beta, color = "red") +
+        #geom_abline(intercept = 0, slope = beta, color = "red") +
+        geom_smooth(method = "lm", se = FALSE, color = "red") +
         geom_smooth(se = FALSE) +
         xlab(labels[j]) +
         ylab(paste0("Component + Residual (", ylabel, ")"))
